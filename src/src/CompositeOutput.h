ce0682dfc932cc98d8dec0e40168d6f012b9fc26
cruwaller <cruwaller@gmail.com>
2021-05-06 11:12:48 +0300

    disable/enable I2S DAC

--- src/src/CompositeOutput.h
+++ src/src/CompositeOutput.h
@@ -190,12 +190,18 @@ class CompositeOutput
 
   void startOutput()
   {
+    i2s_set_dac_mode(I2S_DAC_CHANNEL_RIGHT_EN);
     i2s_start(I2S_PORT);
   }
 
   void stopOutput()
   {
     i2s_stop(I2S_PORT);
+    i2s_set_dac_mode(I2S_DAC_CHANNEL_DISABLE);
+    pinMode(GPIO_NUM_25, INPUT);
+    gpio_pulldown_dis(GPIO_NUM_25);
+    gpio_pullup_dis(GPIO_NUM_25);
+    gpio_set_pull_mode(GPIO_NUM_25, GPIO_FLOATING);
   }
 
   void IRAM_ATTR sendLine()
